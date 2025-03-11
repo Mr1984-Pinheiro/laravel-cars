54128070ec0a805c7059d4c59e5057d2bf1bcacb
carlospinheiro <cadutapinheiro@gmail.com>
2025-03-11 18:28:30 -0300

    layout with template

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Home Page')
+
+@section('content')
+<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates doloremque, impedit id aspernatur dolorem eligendi enim animi, iure deleniti odio est blanditiis illum delectus neque accusantium ipsam exercitationem voluptatum. Deleniti.
+</h1>
+@endsection
